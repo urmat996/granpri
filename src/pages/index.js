496501fc5c66932073c14bff0e1327febe35a13c
urmat996 <71040516+urmat996@users.noreply.github.com>
2020-11-11 02:15:21 +0600

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Box, Icon, Input, Button, Image } from "@quarkly/widgets";
+import { Theme, Link, Text, Button, Box, Icon, Span } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { Override, Menu, Section, StackItem, Stack } from "@quarkly/components";
 import { MdArrowDownward } from "react-icons/md";
+import { BsTools, BsDiamond, BsFillClockFill } from "react-icons/bs";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -22,7 +23,7 @@ export default (() => {
 				flex-direction="row"
 				md-flex-direction="column"
 			>
-				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
+				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--headline3">
 					ГРАН-ПРИ
 				</Text>
 				<Menu
@@ -36,7 +37,16 @@ export default (() => {
 					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
 					<Override slot="link-active" color="--primary" />
 					<Override slot="item" padding="6px" />
+					<Override slot="link-404">
+						8(800)12345678
+					</Override>
+					<Override slot="link-index">
+						ЕКАТЕРИНБУРГ
+					</Override>
 				</Menu>
+				<Button>
+					Вам перезвонить?
+				</Button>
 			</Box>
 		</Section>
 		<Section
@@ -61,13 +71,15 @@ export default (() => {
 					</Text>
 				</StackItem>
 			</Stack>
+			<Button width="20%" margin="20px 0px 0px 0px">
+				ОТПРАВИТЬ ЗАЯВКУ
+			</Button>
 			<Box text-align="center" margin="96px 0 0 0">
 				<Text margin="8px 0" text-transform="uppercase">
 					Наши УСЛУГИ{" "}
 				</Text>
 				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
 			</Box>
-			<Input />
 		</Section>
 		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
 			<Stack margin-top="40px">
@@ -75,7 +87,7 @@ export default (() => {
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/5faad99e79530b001fd24b49/images/11.png?v=2020-11-10T19:18:06.004Z) 50% 0/cover no-repeat" />
 					<Text as="h3" font="--headline3" margin="5px 0 5px 0" text-align="center">
-						МОЙКА АВТО{"\n\n"}
+						МОЙКА АВТО
 					</Text>
 					<Text as="p" margin="0px 0 20px 0" text-align="center">
 						Хотите очистить свой автомобиль,{" "}
@@ -101,13 +113,13 @@ export default (() => {
 						ХИМЧИСТКА САЛОНА
 					</Text>
 					<Text as="p" margin="0px 0 20px 0" text-align="center">
-						Если Вы хотите, чтобы салон Вашего автомобиля значительно посвежел, то мы можем предложить Вам услугу по химчистке салона.{"\n\n"}
+						Если Вы хотите, чтобы салон Вашего автомобиля значительно посвежел, то мы можем предложить Вам услугу по химчистке салона.
 					</Text>
 					<Text as="p" margin="0px 0 20px 0" text-align="center">
 						Стоимость:
 					</Text>
 					<Text as="p" margin="0px 0 30px 0" text-align="center" font="300 46px/24px -apple-system, system-ui, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, Cantarell, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif">
-						1 050 руб.{"\n\n"}
+						1 050 руб.
 					</Text>
 					<Button>
 						ЗАПИСАТЬСЯ
@@ -117,19 +129,18 @@ export default (() => {
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/5faad99e79530b001fd24b49/images/33.png?v=2020-11-10T19:35:32.472Z) 50% 0/cover no-repeat" />
 					<Text as="h3" font="--headline3" margin="5px 0 5px 0" text-align="center">
-						ПОЛИРОВКА МАШИНЫ{"\n\n"}
+						ПОЛИРОВКА МАШИНЫ
 					</Text>
 					<Text as="p" margin="0px 0 20px 0" text-align="center">
 						Важный процесс для подержания внешнего вида транспортного средства в хорошем состоянии.
 						<br />
 						<br />
-						{"\n\n"}
 					</Text>
 					<Text as="p" margin="0px 0 20px 0" text-align="center">
 						Стоимость:
 					</Text>
 					<Text as="p" margin="0px 0 30px 0" text-align="center" font="300 46px/24px -apple-system, system-ui, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, Cantarell, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif">
-						1 600 руб.{"\n\n"}
+						1 600 руб.
 					</Text>
 					<Button>
 						ЗАПИСАТЬСЯ
@@ -137,22 +148,131 @@ export default (() => {
 				</StackItem>
 			</Stack>
 		</Section>
-		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
-			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
-				About Us
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Text
+				as="h1"
+				font="--headline2"
+				md-font="--headline2"
+				margin="20px 0 0 0"
+				text-align="center"
+			>
+				Наши преимущества
 			</Text>
-			<Text as="p" font="--lead" margin="20px 0 0 0">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+			<Stack margin-top="40px">
+				<StackItem width="33%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Icon
+						category="bs"
+						icon={BsTools}
+						size="60px"
+						color="#0069ce"
+						text-align="center"
+						align-items="center"
+						width="100%"
+					/>
+					<Text as="h3" font="--headline3" margin="5px 0 5px 0" text-align="center">
+						Высокое качество
+					</Text>
+					<Text as="p" margin="0px 0 20px 0" text-align="center">
+						обслуживания
+					</Text>
+				</StackItem>
+				<StackItem width="33%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Icon
+						category="bs"
+						icon={BsDiamond}
+						size="60px"
+						color="#0069ce"
+						text-align="center"
+						align-items="center"
+						width="100%"
+					/>
+					<Text as="h3" font="--headline3" margin="5px 0 5px 0" text-align="center">
+						Гарантия
+					</Text>
+					<Text as="p" margin="0px 0 20px 0" text-align="center">
+						низких цен
+					</Text>
+				</StackItem>
+				<StackItem width="33%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Icon
+						category="bs"
+						icon={BsFillClockFill}
+						size="60px"
+						color="#0069ce"
+						text-align="center"
+						align-items="center"
+						width="100%"
+					/>
+					<Text as="h3" font="--headline3" margin="5px 0 5px 0" text-align="center">
+						Сроки
+					</Text>
+					<Text as="p" margin="0px 0 20px 0" text-align="center">
+						исполнения заказов
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			background="--color-primary"
+			display="flex"
+			flex-direction="column"
+		>
+			<Stack color="--grey" font="--base" flex-wrap="wrap" align-items="center">
+				<StackItem flex-grow="1" md-width="100%">
+					<Text
+						as="h1"
+						margin="0px"
+						font="--headline2"
+						md-font="--headline2"
+						color="--light"
+					>
+						ОСТАЛИСЬ ВОПРОСЫ?
+					</Text>
+				</StackItem>
+				<StackItem md-width="100%" font="300 16px/24px -apple-system, system-ui, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, Cantarell, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif">
+					<Button background="#00b41c">
+						<Span font="300 24px/24px -apple-system, system-ui, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, Cantarell, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif">
+							ЗАДАТЬ ВОПРОС
+						</Span>
+					</Button>
+				</StackItem>
+				<StackItem md-width="100%" />
+			</Stack>
+			<Text color="#f4f7ad" position="static" margin="5px 0px 16px 0px">
+				Оставьте заявку и наш менеджер будет рад ответить на Ваши вопросы
 			</Text>
-			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
-				<Box padding="10px">
-					<Image src="https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
-				</Box>
-				<Box padding="10px" />
-				<Box padding="10px">
-					<Image src="https://images.unsplash.com/photo-1503342394128-c104d54dba01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
-				</Box>
-			</Box>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+				<Override slot="link-404">
+					Gran-pri
+				</Override>
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				gran-pri@gmail.com
+			</Link>
 		</Section>
 		<Link
 			font={"--capture"}
